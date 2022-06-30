805fb62c9536852dd1e5b209f024d60a582675c2
Faizan Mirza <fuxxy797@gmail.com>
2022-06-30 13:39:22 +0500

    UI practise project

---
+++ UI-project/uiweb/src/App.js
@@ -0,0 +1,31 @@
+import React from "react";
+import Brand from "./components/brand/Brand";
+import CTA from "./components/cta/CTA";
+import Navbar from "./components/navbar/Navbar";
+import Footer from "./containers/footer/Footer";
+import Header from "./containers/header/Header";
+import Possibility from "./containers/possibility/Possibility";
+import Features from "./containers/feature/Feature";
+import Blog from "./containers/blog/Blog";
+import WhatGPT3 from "./containers/whatGPT3/WhatGPT3";
+import './App.css';
+
+const App = () => {
+    return (
+        <div className="App">
+            <div className="gradient__bg">
+                <Navbar/>
+                <Header/>
+            </div>
+            <Brand/>
+            <WhatGPT3/>
+            <Features/>
+            <Possibility/>
+            <CTA/>
+            <Blog/>
+            <Footer/>
+        </div>
+    );
+} 
+
+export default App;
---
+++ UI-project/uiweb/src/components/cta/CTA.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './cta.css';
+
+const CTA = () => {
+    return (
+        <div>
+            CTA page
+        </div>
+    );
+}
+
+export default CTA;
